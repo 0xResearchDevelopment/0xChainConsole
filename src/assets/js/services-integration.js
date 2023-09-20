b56ab14331e0b04360fa0a91416ba545414f3699
Sadish <sadish003@gmail.com>
2023-09-20 19:14:58 +0530

    Service Integration for SignUp

--- src/assets/js/services-integration.js
+++ src/assets/js/services-integration.js
@@ -1,3 +1,31 @@
+const signup = async() =>  {
+
+     const myBody = {
+        "name":document.getElementById("signup-fullname").value,
+        "email":document.getElementById("signup-email").value,
+        "password":document.getElementById("signup-password").value
+    };
+    console.log(myBody);
+    const response = await fetch('https://euabq2smd3.execute-api.us-east-1.amazonaws.com/dev/api/auth/register', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(myBody)
+    });
+
+    const myJson = await response.json();
+    if(myJson.code == 422) {
+        alert(JSON.stringify(myJson.errors.msg));
+    }
+    else if(myJson.code == 201){
+        alert(JSON.stringify(myJson.message)); 
+        location.href = "sign-up-verification.html";
+    }
+    
+};
+
 function signin() {
     alert("Inside signin");
     location.href=".";
