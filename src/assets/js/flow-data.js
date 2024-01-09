d9d6134c85c08a0459cedd358736dba9814d9a23
SasiKumar Sivaraman <sasi@skpro.local>
2024-01-08 21:12:38 -0500

    option flow data grid enhancements

--- src/assets/js/flow-data.js
+++ src/assets/js/flow-data.js
@@ -33,7 +33,10 @@ var loadFlowData = () => {
                 }
 
                 for (let j = 0; j < tableData.length; j++) {
-                    createTableRows(tableData[j].ticker, tableData[j].type, tableData[j].date_added, tableData[j].added_price, tableData[j].multiplier,
+                    var datevalue = tableData[j].date_added; 
+                    var converteddate = Date.parse(datevalue);
+                    console.log ("### Dates: " + tableData[j].date_added + " - " + converteddate);
+                    createTableRows(tableData[j].ticker, tableData[j].type, converteddate, tableData[j].added_price, tableData[j].multiplier,
                         tableData[j].call_count, tableData[j].call_flow, tableData[j].put_count,tableData[j].put_flow, tableData[j].json_performance,
                         tableData[j].performance, tableData[j].market_cap, tableData[j].company_name);   
                 }
@@ -54,15 +57,17 @@ var loadFlowData = () => {
 
 var createTableRows = (ticker, type, date_added, added_price, multiplier, call_count, call_flow, 
     put_count,put_flow, json_performance, performance, market_cap, company_name) => {
-    const callFlowTheme = call_flow > 0 ? 'success' : 'danger'
-    const callFlowTrend = call_flow > 0 ? 'trending-up' : 'trending-down'
-    const putFlowTheme = put_flow > 0 ? 'success' : 'danger'
-    const putFlowTrend = put_flow > 0 ? 'trending-up' : 'trending-down'
+    const revisedMarketCap = Math.round((market_cap/1000000000)*100)/100;
+    const callFlowTheme = call_flow >= .8 ? 'success' : call_flow < .8 && call_flow >= .5 ? 'info' : 'secondary';
+    const callFlowTrend = call_flow >= .8 &&  call_count >= 20000 && revisedMarketCap >= 20 ? 'bi bi-patch-check-fill text-success ms-1 fs-14' : '';
+    const putFlowTheme = put_flow >= .8 ? 'success' : put_flow < .8 && put_flow >= .5 ? 'info' : 'secondary';
+    const putFlowTrend = put_flow >= .8 &&  put_count >= 20000 && revisedMarketCap >= 20  ? 'bi bi-patch-check-fill text-success ms-1 fs-14' : '';
     const jsonPerformanceTheme = json_performance > 0 ? 'success' : 'danger'
-    const jsonPerformanceTrend = json_performance > 0 ? 'trending-up' : 'trending-down'
+    // const jsonPerformanceTrend = json_performance > 0 ? 'trending-up' : 'trending-down'
     const performanceTheme = performance > 0 ? 'success' : 'danger'
-    const performanceTrend = performance > 0 ? 'trending-up' : 'trending-down'
-    const badgeTheme = (type == 'CALL') ? 'success' : 'secondary';
+    // const performanceTrend = performance > 0 ? 'trending-up' : 'trending-down'
+    const badgeTheme = (type == 'CALL') ? 'primary' : 'secondary';
+    const shorlistedTicker = call_flow >= .8 &&  call_count >= 20000 && revisedMarketCap >= 20 ? 'Yes' : put_flow >= .8 &&  put_count >= 20000 && revisedMarketCap >= 20 ? 'Yes' : '-';
 /* 
     const dateObj = new Date(date_added);
     formatOption = {
@@ -74,17 +79,18 @@ var createTableRows = (ticker, type, date_added, added_price, multiplier, call_c
 
     const row = document.createElement('tr');
     row.innerHTML = `<td style = 'font-size: 12px;'>${ticker}</td>
+    <td style = 'font-size: 12px;'>${shorlistedTicker}</td>
     <td style = 'font-size: 12px;'><span class="badge bg-${badgeTheme}-transparent rounded-pill badge-sm fs-12 fw-semibold">${type}</span></td>
     <td style = 'font-size: 12px;'>${date_added}</td>
     <td style = 'font-size: 12px;'>$${added_price}</td>
-    <td style = 'font-size: 12px;'>${Math.round(multiplier*100)/100}x</td>
+    <td style = 'font-size: 12px;'>${Math.round(multiplier*100)/100}</td>
     <td style = 'font-size: 12px;'>${call_count}</td>
-    <td style = 'font-size: 12px;'><span class='badge bg-${callFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((call_flow*100)*100)/100}%<i class='ti ti-${callFlowTrend} ms-1'></i></span></td>
+    <td style = 'font-size: 12px;'><span class='badge bg-${callFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((call_flow*100)*100)/100}%  <i class='${callFlowTrend}'></i></span></td>
     <td style = 'font-size: 12px;'>${put_count}</td>
-    <td style = 'font-size: 12px;'><span class='badge bg-${putFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((put_flow*100)*100)/100}%<i class='ti ti-${putFlowTrend} ms-1'></i></span></td>
-    <td style = 'font-size: 12px;'>$${Math.round((market_cap/1000000000)*100)/100} B</td>
-    <td style = 'font-size: 12px;'><span class='badge bg-${jsonPerformanceTheme}-transparent fs-12 rounded-pill'>${Math.round((json_performance*100)*100)/100}%<i class='ti ti-${jsonPerformanceTrend} ms-1'></i></span> / 
-                <span class='badge bg-${performanceTheme}-transparent fs-12 rounded-pill'>${Math.round((performance*100)*100)/100}%<i class='ti ti-${performanceTrend} ms-1'></i></span></td>
+    <td style = 'font-size: 12px;'><span class='badge bg-${putFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((put_flow*100)*100)/100}% <i class='${putFlowTrend}'></i></span></td>
+    <td style = 'font-size: 12px;'>$${revisedMarketCap}</td>
+    <td style = 'font-size: 12px;'><span class='badge bg-${jsonPerformanceTheme}-transparent fs-12 rounded-pill'>${Math.round((json_performance*100)*100)/100}%</span> 
+                <span class='badge bg-${performanceTheme}-transparent fs-12 rounded-pill'>${Math.round((performance*100)*100)/100}%</span></td>
     <td style = 'font-size: 12px;'>${company_name}</td>`;
 
     const tbody = document.getElementById("flow-data-tbody");
@@ -98,7 +104,7 @@ var applyResponsiveness = (arrSize) => {
             searchPlaceholder: 'Search...',
             sSearch: ''
         },
-        order: [[0, 'desc']],   //Soring by EventID decensing order
+        order: [[1,'desc']],   //Soring by EventID decensing order
         "pageLength": arrSize,
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
