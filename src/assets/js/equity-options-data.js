08f2e6365f294df72e7b301255d1be698c4fc730
Sadish <sadish003@gmail.com>
2024-01-10 00:34:30 +0530

    Integrated Option flow get API

--- src/assets/js/equity-options-data.js
+++ src/assets/js/equity-options-data.js
@@ -1,48 +1,36 @@
+var targetEndPointUrlBase = 'https://euabq2smd3.execute-api.us-east-1.amazonaws.com/dev';
 
 var loadFlowData = () => {
-    var tableData = [];
-
+    //var tableData = [];
+    const authToken = localStorage.getItem('authToken');
     axios
         .get(
-            'https://presentation.tradealgo.com/ats/historic/daily-darkflow-today?trending_status='
+            targetEndPointUrlBase+'/api/equity/options/loadData',
+            {
+                headers: {
+                    Authorization: `Bearer ${authToken}`
+                }
+            }
         )
         .then(res => {
             if (res.status == 200) {
                 console.log(res.data);
-                const rawData = res.data;
-                var dataObj = '';
-                for (let i = 0; i < rawData.length; i++) {
-                    dataObj = {
-                        "ticker": rawData[i].ticker,
-                        "type": (rawData[i].json_record.options.call_count >= 3000 && rawData[i].json_record.options.call_flow >= 0.55 && rawData[i].json_record.market_cap >= 2000000000) ? 'CALL' : ((rawData[i].json_record.options.put_count >= 3000 && rawData[i].json_record.options.put_flow >= 0.55 && rawData[i].json_record.market_cap >= 2000000000) ? 'PUT' : 'NONE'), //mc,count,flow
-                        "date_added": rawData[i].date_added,
-                        "added_price": rawData[i].added_price,
-                        "multiplier": rawData[i].json_record.multiplier,
-                        "call_count": rawData[i].json_record.options.call_count,
-                        "call_flow": rawData[i].json_record.options.call_flow, //*100 + '%'
-                        "put_count": rawData[i].json_record.options.put_count,
-                        "put_flow": rawData[i].json_record.options.put_flow, //*100 + '%'
-                        "json_performance": rawData[i].json_record.perf, //*100 + '%' -- Stock changes today in $$
-                        "performance": rawData[i].performance, //*100 + '%'
-                        "market_cap": rawData[i].json_record.market_cap,
-                        "company_name": rawData[i].company_name
-                    } 
-                    if(dataObj.type == 'PUT' || dataObj.type == 'CALL'){
-                        tableData.push(dataObj);
+                const optionsData = res.data.equityOptionsData;
+                var tableDataCount = 0;
+                for (let i = 0; i < optionsData.length; i++) {
+                    if(optionsData[i].OPTIONS_TYPE != null){
+                        var formattedDate = Date.parse(optionsData[i].TICKER_ADDED_TS_STR);
+                        var priceChangePercentage = (optionsData[i].PERFORMANCE_1 / optionsData[i].TICKER_PRICE) *100;
+                        createTableRows(optionsData[i].TICKER, optionsData[i].OPTIONS_TYPE, formattedDate, optionsData[i].TICKER_PRICE, optionsData[i].TIKCER_MULTIPLIER,
+                            optionsData[i].OPTIONS_COUNT_CALL, optionsData[i].OPTIONS_FLOW_CALL, optionsData[i].OPTIONS_COUNT_PUT,optionsData[i].OPTIONS_FLOW_PUT, priceChangePercentage,
+                            optionsData[i].PERFORMANCE_2, optionsData[i].MARKET_CAP, optionsData[i].TICKER_NAME);
+                            
+                            tableDataCount = tableDataCount + 1;
                     }              
                 }
 
-                for (let j = 0; j < tableData.length; j++) {
-                    var datevalue = tableData[j].date_added; 
-                    var converteddate = Date.parse(datevalue);
-                    console.log ("### Dates: " + tableData[j].date_added + " - " + converteddate); //TODO: date format fix
-                    var priceChangePercentage = (tableData[j].json_performance / tableData[j].added_price) *100;
-                    createTableRows(tableData[j].ticker, tableData[j].type, datevalue, tableData[j].added_price, tableData[j].multiplier,
-                        tableData[j].call_count, tableData[j].call_flow, tableData[j].put_count,tableData[j].put_flow, priceChangePercentage,
-                        tableData[j].performance, tableData[j].market_cap, tableData[j].company_name);   
-                }
-                applyResponsiveness(tableData.length);
-                showToastAlerts('flow-data-success','alert-success-msg',`Loaded ${tableData.length} records of ${rawData.length}`);
+                applyResponsiveness(optionsData.length);
+                showToastAlerts('equity-options-data-success','alert-success-msg',`Loaded ${tableDataCount} records of ${optionsData.length}`);
             }
         }).catch(err => {
             console.log("inside err");
@@ -58,30 +46,21 @@ var loadFlowData = () => {
 
 var createTableRows = (ticker, type, date_added, added_price, multiplier, call_count, call_flow, 
     put_count,put_flow, json_performance, performance, market_cap, company_name) => {
+    const optionType = type == 1 ? 'CALL' : 'PUT'    
     const revisedMarketCap = Math.round((market_cap/1000000000)*100)/100;
     const callFlowTheme = call_flow >= .8 ? 'success' : call_flow < .8 && call_flow >= .5 ? 'info' : 'secondary';
     const callFlowTrend = call_flow >= .8 &&  call_count >= 20000 && revisedMarketCap >= 20 ? 'bi bi-patch-check-fill text-success ms-1 fs-14' : '';
     const putFlowTheme = put_flow >= .8 ? 'success' : put_flow < .8 && put_flow >= .5 ? 'info' : 'secondary';
     const putFlowTrend = put_flow >= .8 &&  put_count >= 20000 && revisedMarketCap >= 20  ? 'bi bi-patch-check-fill text-success ms-1 fs-14' : '';
     const jsonPerformanceTheme = json_performance > 0 ? 'success' : 'info'; // Stock changes today in $$
-    // const jsonPerformanceTrend = json_performance > 0 ? 'trending-up' : 'trending-down'
     const performanceTheme = performance > 0 ? 'success' : 'info'
-    // const performanceTrend = performance > 0 ? 'trending-up' : 'trending-down'
-    const badgeTheme = (type == 'CALL') ? 'primary' : 'secondary';
+    const badgeTheme = (optionType == 'CALL') ? 'primary' : 'secondary';
     const shorlistedTicker = call_flow >= .69 &&  call_count >= 20000 && revisedMarketCap >= 5 ? 'Yes' : put_flow >= .69 &&  put_count >= 20000 && revisedMarketCap >= 5 ? 'Yes' : '-';  //TODO: $5Billions marketcap
-/* 
-    const dateObj = new Date(date_added);
-    formatOption = {
-    day: 'numeric', month: 'short', year:'numeric',
-    hour: '2-digit', minute: '2-digit',
-    hour12: false
-    timeZone: 'America/Los_Angeles' 
-    }; */
 
     const row = document.createElement('tr');
     row.innerHTML = `<td style = 'font-size: 12px;'>${ticker}</td>
     <td style = 'font-size: 12px;'>${shorlistedTicker}</td>
-    <td style = 'font-size: 12px;'><span class="badge bg-${badgeTheme}-transparent rounded-pill badge-sm fs-12 fw-semibold">${type}</span></td>
+    <td style = 'font-size: 12px;'><span class="badge bg-${badgeTheme}-transparent rounded-pill badge-sm fs-12 fw-semibold">${optionType}</span></td>
     <td style = 'font-size: 12px;'>${Math.round(multiplier*100)/100}</td>
     <td style = 'font-size: 12px;'><span class='badge bg-${callFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((call_flow*100)*100)/100}%  <i class='${callFlowTrend}'></i></span></td>
     <td style = 'font-size: 12px;'><span class='badge bg-${putFlowTheme}-transparent fs-12 rounded-pill'>${Math.round((put_flow*100)*100)/100}% <i class='${putFlowTrend}'></i></span></td>
@@ -94,7 +73,7 @@ var createTableRows = (ticker, type, date_added, added_price, multiplier, call_c
     <td style = 'font-size: 12px;'>${date_added}</td>
     <td style = 'font-size: 12px;'>${company_name}</td>`;
 
-    const tbody = document.getElementById("flow-data-tbody");
+    const tbody = document.getElementById("equity-options-tbody");
     tbody.appendChild(row);
 }
 
